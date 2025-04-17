776909004622cf5a11f27dac11f63ebeeb3b89e5
Josue Mejia (NLS) <josue.mejia@nlsnow.com>
2025-04-17 16:24:16 -0600

    added kubernetes and terraform configuration

---
+++ .infrastructure/terraform/envs/dev/main.tf
@@ -0,0 +1,80 @@
+locals {
+  app_name = "devsu-python-app"
+  cluster_name = "${local.app_name}-${var.environment}"
+}
+
+# data "aws_availability_zones" "available" {
+#   filter {
+#     name   = "opt-in-status"
+#     values = ["opt-in-not-required"]
+#   }
+# }
+
+module "container_registry" {
+
+    source = "../../modules/ecr_repository"
+    repository_name = "${local.app_name}-ecr"
+    tags = {
+        Terraform = true
+        Environment = "development"
+    }
+
+  
+}
+
+# module "vpc" {
+#   source  = "terraform-aws-modules/vpc/aws"
+#   version = "5.19.0"
+
+#   name = "${local.app_name}-vpc"
+
+#   cidr = var.vpc_vars.cidr
+#   azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+
+#   private_subnets = var.vpc_vars.private_subnets
+#   public_subnets  = var.vpc_vars.public_subnets
+
+#   enable_nat_gateway   = true
+#   single_nat_gateway   = true
+#   enable_dns_hostnames = true
+
+#   public_subnet_tags = {
+#     "kubernetes.io/role/elb" = 1
+#   }
+
+#   private_subnet_tags = {
+#     "kubernetes.io/role/internal-elb" = 1
+#   }
+
+#   tags = {
+#     Terraform = "true"
+#     Environment = "development"
+#   }
+# }
+
+# module "eks" {
+#   source  = "terraform-aws-modules/eks/aws"
+#   version = "~> 20.31"
+
+#   cluster_name    = local.cluster_name
+#   cluster_version = "1.31"
+
+#   cluster_endpoint_public_access           = true
+#   enable_cluster_creator_admin_permissions = true
+
+#   cluster_compute_config = {
+#     enabled    = true
+#     node_pools = ["general-purpose"]
+#   }
+
+#   vpc_id     = module.vpc.vpc_id
+#   subnet_ids = module.vpc.private_subnets
+
+#   tags = {
+#     Environment = "development"
+#     Terraform   = "true"
+#   }
+# }
+
+
+
