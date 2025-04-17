47d1755776c14490e56ffca0ca3c7a6a17c28b50
Josue Mejia (NLS) <josue.mejia@nlsnow.com>
2025-04-17 17:01:17 -0600

    updated terraform script and cicd

--- .infrastructure/terraform/envs/dev/variables.tf
+++ .infrastructure/terraform/envs/dev/variables.tf
@@ -5,19 +5,19 @@ variable "region" {
 }
 
 variable "environment" {
-    type = string
-    default = "development"
+  type    = string
+  default = "development"
 }
 
 variable "vpc_vars" {
   type = object({
-    cidr           = string
+    cidr            = string
     private_subnets = list(string)
     public_subnets  = list(string)
   })
 
   default = {
-    cidr           = "10.0.0.0/16"
+    cidr            = "10.0.0.0/16"
     private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
     public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
   }
